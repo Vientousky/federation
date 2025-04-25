b48239e7fa0a9e67e85344610a2fdb933f0fb9d5
Vientousky <jotamancuello2048819@gmail.com>
2025-04-25 19:08:49 -0300

    v9.25

--- frontend/src/app/layout/header/Search.tsx
+++ frontend/src/app/layout/header/Search.tsx
@@ -1,28 +1,7 @@
-"use client";
-
-import { useState, useEffect } from "react";
 import { TbSearch } from "react-icons/tb";
 import styles from "./index.module.css";
 
 const Search: React.FC = () => {
-  const [query, setQuery] = useState("");
-  const [results, setResults] = useState<any>([]);
-
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-        if (query.trim() !== '') {
-            fetch(`/api/busqueda?q=${query}`) 
-                .then(res => res.json())
-                .then(data => {
-                    setResults(data.boxeadores); 
-                });
-        } else {
-            setResults([]);
-        }
-    }, 100);
-
-    return () => clearTimeout(delayDebounceFn);
-}, [query]);
 
   return (
     <>
@@ -33,21 +12,8 @@ const Search: React.FC = () => {
           placeholder="Search..."
           className={styles.search_input}
           aria-label="Search"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
         />
       </form>
-
-      {results.length > 0 && (
-        <div className={styles.search_results}>
-          {results.map((result: any) => (
-            <div key={result.id} className={styles.search_result}>
-              <h3>{result.nombre}</h3>
-              <p>{result.tipo}</p>
-            </div>
-          ))}
-        </div>
-      )}
     </>
   );
 };
