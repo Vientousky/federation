0e53645b1e86d8371198868b340b426f6ed894fc
Vientousky <jotamancuello2048819@gmail.com>
2025-04-25 19:18:12 -0300

    v9.30 por fin andara

---
+++ frontend/src/app/boxeador/[boxeadorId]/interface.ts
@@ -0,0 +1,28 @@
+export interface BoxeadorInfo {
+    id: string;
+    nombre: string;
+    apellidos: string;
+    fecha_nacimiento: string;
+    sexo: string;
+    dni: string;
+    numero_licencia: string;
+    nacionalidad: string;
+    provincia: string;
+    localidad: string;
+    debutaje: string;
+    carrera: string;
+    peso: string;
+    divicion: string; // yase que esta mal escrito
+    status : string;
+    combates: string;
+    rounds: string;
+    KOs: string;
+    altura: string;
+    alcance: string;
+    stance: string;
+    victorias: string;
+    derrotas: string;
+    empates: string;
+    sin_definir: string;
+    foto: string;
+}
--- frontend/src/app/boxeador/[boxeadorId]/page.tsx
+++ frontend/src/app/boxeador/[boxeadorId]/page.tsx
@@ -1,19 +1,23 @@
-async function loadBoxeador(id: string) {
+import { BoxeadorInfo } from './interface'; // Asegúrate de importar la interfaz
+
+async function loadBoxeador(id: string): Promise<BoxeadorInfo> {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/boxeador/${id}/`);
     const data = await res.json();
     return data;
 }
 
-export default async function BoxeadorDetail({params}: {params: Promise<{boxeadorId: string}>}) {
+export default async function BoxeadorDetail({ params }: { params: Promise<{ boxeadorId: string }> }) {
     const { boxeadorId } = await params;
-    const boxeador = await loadBoxeador(boxeadorId);
+    const boxeador: BoxeadorInfo = await loadBoxeador(boxeadorId);
 
     return (
         <main>
             <h1>Perfil {boxeadorId}</h1>
             <div>
-                <h2>{boxeador.nombre}</h2>
-                <p>{boxeador.descripcion}</p>
+                <h2>{boxeador.nombre} {boxeador.apellidos}</h2>
+                <p>Fecha de nacimiento: {boxeador.fecha_nacimiento}</p>
+                <p>Sexo: {boxeador.sexo}</p>
+                {/* Agrega más detalles del boxeador aquí */}
             </div>
         </main>
     );
