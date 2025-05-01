899395643ae0612c1f6a870a3185ce816450a996
Vientousky <jotamancuello2048819@gmail.com>
2025-05-01 19:53:34 -0300

    v10.12

--- backend/combates/models.py
+++ backend/combates/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Combates(models.Model):
+    fecha = models.DateTimeField()
+    peso = models.FloatField()
+    nombre = models.CharField(max_length=100)
+    lugar = models.CharField (max_length=200)
+    resultados
+    rounds
+    v_l_d
+    ultimas_6
