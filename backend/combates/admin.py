e98f4027186e686bf480d6adaf76dd0cf18ab97b
Vientousky <jotamancuello2048819@gmail.com>
2025-05-03 13:29:47 -0300

    v11.05

--- backend/combates/admin.py
+++ backend/combates/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from .models import Combates
 
 # Register your models here.
 
 class CombatesAdmin(admin.ModelAdmin):
-    list_display= ('boxeador','rival','peso','fecha','lugar','resultados' )
+    list_display= ('boxeador','rival','peso','fecha','lugar','resultados')
+
+admin.site.register(Combates, CombatesAdmin)
